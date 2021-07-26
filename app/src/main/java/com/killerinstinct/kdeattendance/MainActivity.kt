3c194b91e8b105584f4776bb49ed84285114d824
Vibin <vibin.cr22@gmail.com>
2021-07-26 18:32:17 +0530

    Scrolling works but recyclerview doesn't

--- app/src/main/java/com/killerinstinct/kdeattendance/MainActivity.kt
+++ app/src/main/java/com/killerinstinct/kdeattendance/MainActivity.kt
@@ -9,8 +9,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.main_recyclerview)
-        recyclerView.adapter = MainRecyclerAdapter(Utils.employeeList)
+        val employeeList = listOf(
+            Employee(1,"Vibin",25),
+            Employee(2,"Nitin",25),
+            Employee(3,"Nitin",25),
+            Employee(4,"Nitin",25),
+            Employee(5,"Nitin",25),
+            Employee(6,"Nitin",25),
+            Employee(7,"Nitin",25),
+            Employee(8,"Nitin",25),
+        )
+//        val recyclerView = findViewById<RecyclerView>(R.id.main_recyclerview)
+//        recyclerView.adapter = MainRecyclerAdapter(employeeList)
 
     }
 }
