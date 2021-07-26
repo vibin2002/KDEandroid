f27a66a6ac09123c92f0a27d0029f67262ae3d84
SanthiyaM-code <santhiya10082001@gmail.com>
2021-07-26 22:24:44 +0530

    Scrollview

--- app/src/main/java/com/killerinstinct/kdeattendance/Utils.kt
+++ app/src/main/java/com/killerinstinct/kdeattendance/Utils.kt
@@ -4,13 +4,14 @@ object Utils {
 
     val employeeList = listOf(
         Employee(1,"John snow",25),
-        Employee(2,"John snow",35),
-        Employee(3,"John snow",45),
-        Employee(4,"John snow",15),
-        Employee(5,"John snow",25),
-        Employee(6,"John snow",95),
+        Employee(2,"Tyrion",35),
+        Employee(3,"Myrcella",45),
+        Employee(4,"Cersei",15),
+        Employee(5,"Robert",25),
+        Employee(6,"Dragon",95),
         Employee(7,"John snow",95),
         Employee(8,"John snow",75),
+        Employee(8,"Dany",100)
     )
 
 
